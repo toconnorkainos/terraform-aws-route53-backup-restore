abd772b634f3a46916b0edbbe3b5c863c6197d58
Tom O'Connor <t.oconnor@kainos.com>
2021-07-23 16:24:11 +0100

    Tested backup version with logging on error.

--- code/backup_route53.py
+++ code/backup_route53.py
@@ -5,9 +5,10 @@ from datetime import datetime
 import boto3
 from botocore.exceptions import ClientError
 import route53_utils
+import logging
 
-bucket_name = os.environ.get("S3_BUCKET_NAME")
-aws_region = os.environ.get("AWS_REGION")
+bucket_name = os.environ.get("S3_BUCKET_NAME", None)
+aws_region = os.environ.get("AWS_REGION", "eu-west-2")
 s3 = boto3.client('s3')
 route53 = boto3.client('route53')
 
@@ -27,6 +28,10 @@ def get_route53_hosted_zones(next_dns_name=None, next_hosted_zone_id=None):
 
 
 def handle(event, context):
+    if bucket_name is None:
+        logging.error("S3_BUCKET_NAME env var must be set")
+        raise EnvironmentError("S3_BUCKET_NAME env var must be set")
+    
     timestamp = time.strftime("%Y-%m-%dT%H:%M:%SZ", datetime.utcnow().utctimetuple())
 
     hosted_zones = get_route53_hosted_zones()
